1495eb99194ffeb28df17ca49fc22f4605ff8f95
luizzdre <luizandrecps@gmail.com>
2023-03-08 08:39:45 -0300

    aula sobre javascript com funcoes e variaveis

---
+++ aula02/funçoesevariaveis.js
@@ -0,0 +1,14 @@
+let variavel3 = 3
+const variavel2 = 9
+var variavel1 = "ola"
+const obj = {
+  nome:"luiz",
+  signo: "cancer"
+}
+// Formato json - javaScript NOTATION Object
+console.log (variavel1)
+console.log (variavel2 + variavel3)
+console.log (typeof variavel1)
+console.log (typeof variavel2)
+console.log (obj)
+
